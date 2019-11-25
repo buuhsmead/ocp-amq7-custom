469c5db05bd35160e9b89934170000208c27b999
Huub Daems <hdaems@redhat.com>
2019-11-25 15:33:55 +0100

    Added TLS route external 5.

--- configuration/pre_launch.sh
+++ configuration/pre_launch.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-echo "PRE LAUNCH START : ${INSTANCE_DIR}"
-
 BROKER_DIR=/home/jboss/${AMQ_NAME}
 
+echo "PRE LAUNCH START : ${BROKER_DIR}"
 
 function replaceVars() {
   sed -i "s/\${LDAP_BIND_PASSWORD}/$LDAP_BIND_PASSWORD/g" $1
+  sed -i "s/\${role}/$AMQ_ROLE/g" $1
+  sed -i "s/\${user}/$AMQ_USER/g" $1
+
 }
 
 replaceVars $BROKER_DIR/etc/login.config
