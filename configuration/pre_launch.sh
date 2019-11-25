17dd1e7be87831b5a867a0f837c11687c1b6385b
Huub Daems <hdaems@redhat.com>
2019-11-25 15:13:17 +0100

    Added TLS route external 4.

--- configuration/pre_launch.sh
+++ configuration/pre_launch.sh
@@ -2,12 +2,12 @@
 
 echo "PRE LAUNCH START : ${INSTANCE_DIR}"
 
-#INSTANCE_DIR=/home/jboss/${AMQ_NAME}
+BROKER_DIR=/home/jboss/${AMQ_NAME}
 
 
 function replaceVars() {
   sed -i "s/\${LDAP_BIND_PASSWORD}/$LDAP_BIND_PASSWORD/g" $1
 }
 
-replaceVars $INSTANCE_DIR/etc/login.config
+replaceVars $BROKER_DIR/etc/login.config
 
