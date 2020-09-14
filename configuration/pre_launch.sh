744cc886b40e361e85f2d27b7296b29c51a332d3
Huub Daems <hdaems@redhat.com>
2020-09-14 13:37:22 +0200

    vp adddd

--- configuration/pre_launch.sh
+++ configuration/pre_launch.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-BROKER_DIR=/home/jboss/${AMQ_NAME}
+# have to find another way of doing this, maybe an initContainer
+
+
+BROKER_DIR="/home/jboss/${AMQ_NAME}"
 
 echo "PRE LAUNCH START : ${BROKER_DIR}"
 
@@ -8,19 +11,35 @@ function replaceVars() {
   sed -i "s/\${LDAP_BIND_PASSWORD}/$LDAP_BIND_PASSWORD/g" $1
   sed -i "s/\${role}/$AMQ_ROLE/g" $1
   sed -i "s/\${user}/$AMQ_USER/g" $1
-
+  sed -i "s/\${BROKER_IP}/0.0.0.0/g" $1
+  sed -i "s/\${AMQ_KEYSTORE_PASSWORD}/$AMQ_KEYSTORE_PASSWORD/g" $1
+  sed -i "s/\${AMQ_NAME}/$AMQ_NAME/g" $1
+  sed -i "s]\${AMQ_DATA_DIR}]$AMQ_DATA_DIR]g" $1
 }
 
-replaceVars $BROKER_DIR/etc/login.config
 
 
+# Remove all 'security-settings'
+sed -i '/<security-settings>/,/<\/security-settings>/d' "$BROKER_DIR/etc/broker.xml"
+# Remove all 'address-settings'
+sed -i '/<address-settings>/,/<\/address-settings>/d' "$BROKER_DIR/etc/broker.xml"
+# Remove all 'addresses'
+sed -i '/<addresses>/,/<\/addresses>/d' "$BROKER_DIR/etc/broker.xml"
+# Insert previously removed XML as include
+sed -i '/<\/core>/i  <xi:include href="/home/jboss/broker-extra-config/address.xml"/><xi:include href="/home/jboss/broker-extra-config/security.xml"/><xi:include href="/home/jboss/broker-extra-config/addresses.xml"/>' "$BROKER_DIR/etc/broker.xml"
 
-sed -i '/<security-settings>/,/<\/security-settings>/d' $BROKER_DIR/etc/broker.xml
+FILES="$BROKER_DIR/etc/*"
 
-sed -i '/<address-settings>/,/<\/address-settings>/d' $BROKER_DIR/etc/broker.xml
+# pre_launch.sh MUST NOT be replaced vars
 
-sed -i '/<addresses>/,/<\/addresses>/d' $BROKER_DIR/etc/broker.xml
+for item in $FILES
+do
+	if [[ "$(basename $item)" != "pre_launch.sh" ]]
+	then
+		replaceVars $item
+	fi
+done
 
-sed -i '/<\/core>/i  <xi:include href="/home/jboss/broker-extra-config/address.xml"/><xi:include href="/home/jboss/broker-extra-config/security.xml"/><xi:include href="/home/jboss/broker-extra-config/addresses.xml"/>' $BROKER_DIR/etc/broker.xml
+
 
 touch $BROKER_DIR/etc/broker.xml
